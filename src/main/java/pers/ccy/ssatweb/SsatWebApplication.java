5e97241203e9df5c78df840550dff01b1693e1b5
desperadoccy <527707607@qq.com>
2020-04-27 21:24:30 +0800

    初始化项目

---
+++ src/main/java/pers/ccy/ssatweb/SsatWebApplication.java
@@ -0,0 +1,13 @@
+package pers.ccy.ssatweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsatWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsatWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pers/ccy/ssatweb/SsatWebApplicationTests.java
@@ -0,0 +1,13 @@
+package pers.ccy.ssatweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsatWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

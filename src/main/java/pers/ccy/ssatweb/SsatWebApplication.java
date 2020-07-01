07359b703c24c9aa26b9cb4e81f376b4f329feb1
desperadoccy <527707607@qq.com>
2020-07-01 14:56:09 +0800

    整合了swagger-ui

--- src/main/java/pers/ccy/ssatweb/SsatWebApplication.java
+++ src/main/java/pers/ccy/ssatweb/SsatWebApplication.java
@@ -2,7 +2,9 @@ package pers.ccy.ssatweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
 public class SsatWebApplication {
 
